b9a7c658ad296599d084b5791edb7c6a7d97b23b
Chris <chrismjboyle@gmail.com>
2019-06-20 15:50:28 +0100

    Initial commit; added small stuff

---
+++ src/rheo.c
@@ -0,0 +1,17 @@
+#include "rheo.h"
+
+int
+main (int argc, const char ** argv)
+{
+  // parse arguments
+  //    how long?
+  //    tag?
+  //    control scheme?
+  
+  // set up stuff
+  //    sensor threads
+  //    logging thread
+  //    display (main) thread
+
+  return 0;
+}
---
+++ src/rheo_adc.c
@@ -0,0 +1,5 @@
+double
+read_adc_value(unsigned int channel)
+{
+  // TODO: need to find out how to SPI in c
+}
