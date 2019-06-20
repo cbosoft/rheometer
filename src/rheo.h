07c1d23b10b40901aec96a3154dbe88dc4abdcf4
Chris <chrismjboyle@gmail.com>
2019-06-20 16:50:03 +0100

    Add adc code

--- src/rheo.h
+++ src/rheo.h
@@ -1,1 +1,28 @@
 #pragma once
+
+#define VERSION "0.1"
+
+// rheo_error.c
+void rheo_ferr(const char *mesg);
+void rheo_warn(const char *mesg);
+
+// rheo_adc.c {{{
+
+typedef struct rheo_adc_ctrl {
+  const char *device;
+  int fd;
+  unsigned int mode;
+  unsigned int bits;
+  unsigned int speed;
+  unsigned int delay;
+} rheo_adc_ctrl;
+
+
+rheo_adc_ctrl *rheo_adc_open(const char *device);
+void rheo_adc_close(rheo_adc_ctrl *h);
+unsigned int rheo_read_adc_value(rheo_adc_ctrl *h, unsigned int channel);
+
+// }}}
+
+
+// vim: foldmethod=marker ft=c
--- src/rheo_adc.c
+++ src/rheo_adc.c
@@ -1,5 +1,75 @@
-double
-read_adc_value(unsigned int channel)
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+
+#include "rheo.h"
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+
+unsigned int
+rheo_read_adc_value(rheo_adc_ctrl *h, unsigned int channel)
 {
-  // TODO: need to find out how to SPI in c
+
+  int ret;
+  uint8_t tx[] = {
+    4 + (channel>>2), (channel&3)<<6, 0
+  };
+  unsigned int L = ARRAY_SIZE(tx);
+  uint8_t rx[] = {0, 0, 0};
+
+  struct spi_ioc_transfer tr = {
+    .tx_buf = (unsigned long)tx,
+    .rx_buf = (unsigned long)rx,
+    .len = L,
+    .delay_usecs = h->delay,
+    .speed_hz = h->speed,
+    .bits_per_word = h->bits,
+  };
+
+  ret = ioctl(h->fd, SPI_IOC_MESSAGE(1), &tr);
+  if (ret < 1)
+    rheo_ferr("can't send spi message");
+  
+  unsigned int total = 0;
+  for (unsigned int i = 0; i < L; i++) {
+    total += rx[i];
+  }
+
+  return total;
+}
+
+
+
+rheo_adc_ctrl *
+rheo_adc_open(const char *device)
+{
+  int fd = open(device, O_RDWR);
+  if (fd < 0)
+    rheo_ferr("could not open spi device");
+
+  rheo_adc_ctrl *h = malloc(sizeof(rheo_adc_ctrl));
+
+  h->device = device;
+  h->fd = fd;
+  
+  // default values
+  h->delay = 0;
+  h->speed = 500000;
+  h->bits = 8;
+
+  return h;
+}
+
+
+
+void
+rheo_adc_close(rheo_adc_ctrl *h)
+{
+  close(h->fd);
+  free(h);
 }
