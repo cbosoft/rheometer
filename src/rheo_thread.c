912009962ee9df9f21e7d78f837fbd3795e814bf
Christopher <christopher.boyle.101@strath.ac.uk>
2019-06-21 12:51:38 +0100

    Added ADC and LOG threads

--- src/rheo_thread.c
+++ src/rheo_thread.c
@@ -1,4 +1,6 @@
+#define _POSIX_C_SOURCE 199309L
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <time.h>
 #include <sys/time.h>
@@ -12,13 +14,15 @@ new_thread_data()
   
   rv->time_s = malloc(sizeof(unsigned long));
   rv->time_us = malloc(sizeof(unsigned long));
-  rv->adc = malloc(ADC_COUNT * sizeof(float *));
-  for (int i = 0; i < ADC_COUNT; i++)
-    rv->adc[i] = malloc(sizeof(float));
+  rv->adc = malloc(ADC_COUNT * sizeof(float));
   rv->temperature = malloc(sizeof(float));
 
   rv->stopped = 0;
   rv->errored = 0;
+  rv->adc_ready = 0;
+  rv->opt_ready = 0;
+  rv->log_ready = 0;
+  rv->tmp_ready = 0;
   rv->error_string = "all is well";
   return rv;
 }
@@ -29,13 +33,24 @@ new_thread_data()
 void
 free_thread_data(thread_data *dat)
 {
+  adc_close(dat->adc_h);
+  free_run_data(dat->run_d);
   free(dat->time_s);
   free(dat->time_us);
-  for (int i = 0; i < ADC_COUNT; i++)
-    free(dat->adc[i]);
   free(dat->adc);
   free(dat->temperature);
   free(dat);
+}
+
+
+
+void
+nsleep(unsigned int delay_ns)
+{
+  struct timespec delay, res;
+  delay.tv_sec = 0;
+  delay.tv_nsec = delay_ns;
+  nanosleep(&delay, &res);
 }
 
 
@@ -46,10 +61,27 @@ log_thread_func(void *vt_d) {
   thread_data *t_d = (thread_data *)vt_d;
   
   struct timeval tv;
-  struct timespec delay, res;
-  delay.tv_sec = 0;
-  delay.tv_nsec = 10000;
   unsigned long *sec, *usec, *psec, *pusec;
+  
+  char *logpref = "logs/rpir";
+  char *logsuff = ".csv";
+  char *date = calloc(50, sizeof(char));
+  char *controlscheme = "constantXX";
+  time_t rawtime;
+  struct tm *timeinfo;
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  // TODO: replace hour-minute-second with (count)
+  strftime(date, 50, "%Y-%m-%d-%H-%M-%S", timeinfo);
+  
+  char *logpath = calloc(256, sizeof(char));
+  sprintf(logpath, "%s_%s_%s_%s%s", logpref, date, controlscheme, t_d->run_d->tag, logsuff);
+
+  fprintf(stderr, "Logging initialised; log file \033[34;1m\"%s\"\033[0m\n", logpath);
+
+  FILE *log_fp = fopen(logpath, "w");
+
+  t_d->log_ready = 1;
   while ( (!t_d->stopped) && (!t_d->errored) ) {
     // set the time
     gettimeofday(&tv, 0);
@@ -63,9 +95,48 @@ log_thread_func(void *vt_d) {
     t_d->time_us = usec;
     free(psec);
     free(pusec);
+    
+    fprintf(log_fp, "%lu.%06lu,", (*sec), (*usec));
+    for (unsigned int channel = 0; channel < ADC_COUNT; channel++) {
+      fprintf(log_fp, "%lu,", t_d->adc[channel]);
+    }
+    fprintf(log_fp, "%f\n", (*t_d->temperature));
 
-    nanosleep(&delay, &res);
+    
+    nsleep(100000);
   }
+
+  fclose(log_fp);
   
+  return NULL;
+}
+
+
+
+void *
+adc_thread_func(void *vt_d) {
+  thread_data *t_d = (thread_data *)vt_d;
+  adc_handle *adc_h = t_d->adc_h;
+  
+  unsigned long *adc, *padc;
+
+  t_d->adc_ready = 1;
+  while ( (!t_d->stopped) && (!t_d->errored) ) {
+    
+    adc = malloc(ADC_COUNT*sizeof(unsigned long));
+    for (unsigned int channel = 0; channel < ADC_COUNT; channel++) {
+      adc[channel] = read_adc_value(adc_h, channel);
+    }
+    
+    padc = t_d->adc;
+    t_d->adc = adc;
+
+    free(padc);
+
+    nsleep(10000);
+
+  }
+
+
   return NULL;
 }
