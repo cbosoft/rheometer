a003b0ed8696c7f26821066fda7abb2b8268335d
Christopher <christopher.boyle.101@strath.ac.uk>
2019-06-21 12:49:49 +0100

    Displays proper data, init log and adc thread

--- src/rheo.c
+++ src/rheo.c
@@ -14,42 +14,61 @@ void
 inthandle(int signo)
 {
   if (signo == SIGINT) {
-    fprintf(stderr, "\rctrl-c pressed!\n");
+    fprintf(stderr, "\nctrl-c pressed!\n");
     cancelled = 1;
   }
 }
 
 void
 display_thread_data(thread_data *t_d) {
-  fprintf(stderr, "%lu.%06lu \n", (*t_d->time_s), (*t_d->time_us));
+  fprintf(stderr, "t = %lu.%06lu ", (*t_d->time_s), (*t_d->time_us));
+
+  for (unsigned int channel = 0; channel < ADC_COUNT; channel++) {
+    fprintf(stderr, "%5lu ", t_d->adc[channel]);
+  }
+
+  fprintf(stderr, "%f\n", (*t_d->temperature));
 }
 
 
 int
 main (int argc, const char ** argv)
 {
-  run_data *r_d = parse_args(argc, argv);
-  thread_data *t_d = new_thread_data();
-  //pthread_t *optenc_threads = setup_optenc_threads();
-  pthread_t log_thread;
+  if (signal(SIGINT, inthandle) == SIG_ERR)
+    ferr("could not create signal handler");
 
+  thread_data *t_d = new_thread_data();
+  t_d->adc_h = adc_open("/dev/spidev0.1");
+  t_d->run_d = parse_args(argc, argv);
+  
+  //pthread_t *optenc_threads = setup_optenc_threads();
+
+  pthread_t log_thread;
   if (pthread_create(&log_thread, NULL, log_thread_func, t_d))
     ferr("could not create log thread");
 
-  if (signal(SIGINT, inthandle) == SIG_ERR)
-    exit(1);
-  
+  pthread_t adc_thread;
+  if (pthread_create(&adc_thread, NULL, adc_thread_func, t_d))
+    ferr("could not create adc thread");
+
+  while (!t_d->log_ready) nsleep(100);
+  while (!t_d->adc_ready) nsleep(100);
+  //while (!t_d->tmp_ready) nsleep(100);
+  //while (!t_d->opt_ready) nsleep(100);
+
+
   while (!cancelled) {
-    display_thread_data(t_d);
     sleep(1);
+    display_thread_data(t_d);
   }
 
   t_d->stopped = 1;
   
   if (pthread_join(log_thread, NULL))
-    ferr("could not join thread");
+    ferr("log_thread could not rejoin");
+  if (pthread_join(adc_thread, NULL))
+    ferr("adc_thread could not rejoin");
 
   free_thread_data(t_d);
-  free_run_data(r_d);
   return 0;
 }
