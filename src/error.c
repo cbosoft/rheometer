575c95bef1fbab1ed22b14b78c0b42cf9aadbb0a
Chris <chrismjboyle@gmail.com>
2019-06-23 11:43:37 +0100

    Arg error to err.c

--- src/error.c
+++ src/error.c
@@ -8,16 +8,24 @@
 void
 ferr (const char* mesg)
 {
-  fprintf(stderr, "\033[31mFATAL ERROR!\033[0m : %s\n", mesg);
+  fprintf(stderr, "\033[31mFATAL ERROR!\033[0m %s\n", mesg);
   if (errno)
     fprintf(stderr, "  (%d)  %s\n", errno, strerror(errno));
   exit(1);
 }
 
 void
+argerr(const char* mesg)
+{
+  fprintf(stderr, "\033[31mUSAGE ERROR!\033[0m %s\n", mesg);
+  usage();
+  exit(1);
+}
+
+void
 warn (const char* mesg)
 {
-  fprintf(stderr, "\033[33mWARNING!\033[0m : %s\n", mesg);
+  fprintf(stderr, "\033[33mWARNING!\033[0m %s\n", mesg);
 }
 
 void
