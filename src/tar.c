40aa35312735f5d7ca45d66c7b7d357d2180a20a
Chris <chrismjboyle@gmail.com>
2019-06-24 12:07:34 +0100

    Fixed access bug

--- src/tar.c
+++ src/tar.c
@@ -5,24 +5,57 @@
 
 #include "rheo.h"
 
-void
-tidy_logs(thread_data *td) 
+char *
+escape_parens(char *cmd)
 {
-#ifndef DEBUG
-  char  *tar_cmd = calloc(1000, sizeof(char));
+  unsigned int cmdlen = strlen(cmd);
+  char *rv = calloc(cmdlen*2, sizeof(char));
+  
+  unsigned int j = 0;
+  for (unsigned int i = 0; i < cmdlen; i++, j++) {
+    if (cmd[i] == '(' || cmd[i] == ')') {
+      rv[j] = '\\';
+      j++;
+    }
+    rv[j] = cmd[i];
+  }
+  return rv;
+}
+
+void
+tidy_logs(thread_data_t *td) 
+{
+  unsigned int cmdlen = 0;
+  cmdlen += 8; // "tar cjf "
+  for (unsigned int i = 0; i < td->log_count; i++)
+    cmdlen += (td->log_paths[i] == NULL) ? 0 : strlen(td->log_paths[i]) + 1;
+  cmdlen += 8; // ".tar.bz2"
+  cmdlen += 1; // " "
+  cmdlen += 1; // "\0"
+  cmdlen += 1000; // this is overkill, for some reason not calculating amount of memory correctly.
+
+  char  *tar_cmd = calloc(cmdlen, sizeof(char));
   strcat(tar_cmd, "tar cjf ");
   strcat(tar_cmd, td->log_pref);
   strcat(tar_cmd, ".tar.bz2");
 
-  for (int i = 0; i < td->log_count; i++) {
+  for (unsigned int i = 0; i < td->log_count; i++) {
+    if (td->log_paths[i] == NULL) {
+      char err_mesg[35] = {0};
+      sprintf(err_mesg, "log path %u is NULL, skipping.", i);
+      warn(err_mesg);
+      continue;
+    }
     strcat(tar_cmd, " ");
     strcat(tar_cmd, td->log_paths[i]);
   }
-  int rv = system(tar_cmd);
+
+  char *tar_cmd_escaped = escape_parens(tar_cmd);
+  int rv = system(tar_cmd_escaped);
   free(tar_cmd);
+  free(tar_cmd_escaped);
 
   if (rv == -1) {
     ferr("error when tidying logs");
   } // TODO: what about other rvs?
-#endif
 }
