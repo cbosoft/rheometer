fcd6877ab35e257d2c9f7de234e185a145b6389b
Christopher <christopher.boyle.101@strath.ac.uk>
2019-06-25 12:53:44 +0100

    Freeing some alloc

--- src/thread.c
+++ src/thread.c
@@ -38,7 +38,6 @@ init(int argc, const char **argv, thread_data_t *td)
 {
   const char *log_dir = "logs";
   const char *genpref = "rpir";
-  char *controlscheme = "constant-XX";
 
   char *date = calloc(15, sizeof(char));
   time_t rawtime;
@@ -48,14 +47,14 @@ init(int argc, const char **argv, thread_data_t *td)
   strftime(date, 50, "%Y-%m-%d", timeinfo);
 
   char *pattern = calloc(256, sizeof(char));
-  sprintf(pattern, "%s/%s_%s(*)_%s_%s.csv", log_dir, genpref, date, controlscheme, td->tag);
+  sprintf(pattern, "%s/%s_%s(*)_%s_%s.csv", log_dir, genpref, date, td->control_scheme, td->tag);
 
   glob_t glob_res;
   glob((const char *)pattern, GLOB_NOSORT, NULL, &glob_res);
   free(pattern);
 
   char *log_pref = calloc(256, sizeof(char));
-  sprintf(log_pref, "%s/%s_%s(%u)_%s_%s", log_dir, genpref, date, (unsigned int)glob_res.gl_pathc, controlscheme, td->tag);
+  sprintf(log_pref, "%s/%s_%s(%u)_%s_%s", log_dir, genpref, date, (unsigned int)glob_res.gl_pathc, td->control_scheme, td->tag);
   td->log_pref = log_pref;
   td->log_paths = calloc(10, sizeof(char *));
   td->log_count = 0;
@@ -64,6 +63,8 @@ init(int argc, const char **argv, thread_data_t *td)
   td->ptimes = calloc(OPTENC_COUNT, sizeof(float *));
   for (unsigned int i = 0; i < OPTENC_COUNT; i++)
     td->ptimes[i] = calloc(SPD_HIST, sizeof(float *));
+
+  free(date);
 }
 
 
@@ -76,6 +77,7 @@ free_thread_data(thread_data_t *td)
   for (unsigned int i = 0; i < td->log_count; i++)
     free(td->log_paths[i]);
   free(td->time_s);
+  free(td->log_pref);
   free(td->time_us);
   for (unsigned int i = 0; i < OPTENC_COUNT; i++)
     free(td->ptimes[i]);
