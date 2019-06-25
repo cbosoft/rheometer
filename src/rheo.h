d475c529ec2b86648c75217676046162abbd8ce5
Chris <chrismjboyle@gmail.com>
2019-06-25 09:43:26 +0100

    Added colour escape defines

--- src/rheo.h
+++ src/rheo.h
@@ -9,6 +9,27 @@
 #define OPTENC_COUNT 3
 #define PWM_PIN 18
 
+#define BOLD  "\033[1m"
+#define RESET "\033[0m"
+#define FGBLACK   "\033[30m"
+#define FGRED     "\033[31m"
+#define FGGREEN   "\033[32m"
+#define FGYELLOW  "\033[33m"
+#define FGBLUE    "\033[34m"
+#define FGMAGENTA "\033[35m"
+#define FGCYAN    "\033[36m"
+#define FGWHITE   "\033[37m"
+
+#define BGBLACK   "\033[40m"
+#define BGRED     "\033[41m"
+#define BGGREEN   "\033[42m"
+#define BGYELLOW  "\033[43m"
+#define BGBLUE    "\033[44m"
+#define BGMAGENTA "\033[45m"
+#define BGCYAN    "\033[46m"
+#define BGWHITE   "\033[47m"
+#define TAGDEFAULT "DELME"
+
 // }}}
 // types, structs, and enums {{{
 
