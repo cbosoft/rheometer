69662fa3465954cb7ce6957efe4f72d41991d74d
Chris <chrismjboyle@gmail.com>
2019-06-26 08:54:27 +0100

    Added arg --depth, moved control scheme parsing over to control.c

--- src/args.c
+++ src/args.c
@@ -1,14 +1,10 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
-#include "cJSON.h"
 #include "rheo.h"
 
 #define EITHER(A,B,C) ( (strcmp(A, B) == 0) || (strcmp(A, C) == 0) )
-
-
 
 
 void
@@ -19,7 +15,7 @@ usage(void)
       "  "BOLD"rheometer"RESET" control program v"VERSION"\n"
       "\n"
       "  Usage:\n"
-      "    rheometer -l|--length <length> -c|--control-scheme <control scheme> [-t|--tag <tag>]\n"
+      "    rheometer -l <length> -d <depth> -c <control scheme> [-t <tag>]\n"
       "    rheometer -h|--help\n"
       "\n"
   );
@@ -37,6 +33,9 @@ help(void)
       "    -l | --length    Length of run, can be given in seconds or minutes, dictated\n"
       "                     by a suffixed character. e.g. \"10s\" is 10 seconds, \"10m\"\n"
       "                     is 10 minutes. If ignored, the value is taken as seconds.\n"
+      "\n"
+      "    -d | --depth     Fill depth of Couette cell. This is the height fluid extends \n"
+      "                     up on the "BOLD"inner"RESET" cylinder, in mm.\n"
       "\n"
       "    -c | --control-scheme    Control scheme JSON file path. The JSON object\n"
       "                     describes the control scheme to use in the run. Each control\n"
@@ -94,166 +93,6 @@ parse_length_string(const char *length_s_str)
 
 
 void
-get_control_scheme_parameter(cJSON *json, const char *paramname, const char *schemename, const char *description, double *value)
-{
-  cJSON *dbl = cJSON_GetObjectItem(json, paramname);
-
-  if (dbl == NULL) {
-    char err_mesg[1000] = {0};
-    sprintf(err_mesg, "%s requires a parameter \"%s\" (%s)", schemename, paramname, description);
-    cJSON_Delete(json);
-    ferr(err_mesg);
-  }
-
-  if (!cJSON_IsNumber(dbl)) {
-    char err_mesg[1000] = {0};
-    sprintf(err_mesg, "%s requires \"%s\" to be a number (%s)", schemename, paramname, description);
-  }
-  
-  (*value) = dbl->valuedouble;
-}
-
-
-
-
-void
-get_optional_control_scheme_parameter(cJSON *json, const char *paramname, const char *description, double *dbl_value, unsigned int *int_value, char **str_value)
-{
-  cJSON *param = cJSON_GetObjectItem(json, paramname);
-
-  if (param == NULL)
-    return;
-
-  if (cJSON_IsNumber(param)) {
-
-    if (dbl_value == NULL) {
-
-      if (int_value == NULL) {
-        char err_mesg[1000] = {0};
-        sprintf(err_mesg, "JSON parse error. \"%s\" is not expected to be a number (%s).", paramname, description);
-        ferr(err_mesg);
-      }
-      else {
-        (*int_value) = param->valueint;
-      }
-
-    }
-    else {
-      (*dbl_value) = param->valuedouble;
-    }
-
-  } 
-  else if (cJSON_IsString(param)) {
-
-    if (str_value == NULL) {
-        char err_mesg[1000] = {0};
-        sprintf(err_mesg, "JSON parse error. \"%s\" is not expected to be a string (%s).", paramname, description);
-        ferr(err_mesg);
-    }
-    else {
-      (*str_value) = param->valuestring;
-    }
-  }
-}
-
-
-
-void
-parse_contol_scheme_string(thread_data_t *td, const char *control_scheme_string)
-{
-  info("reading control scheme");
-
-  if (access(control_scheme_string, F_OK) == -1) {
-    argerr("control scheme must be a json file describing the scheme.");
-  }
-
-  FILE *fp = fopen(control_scheme_string, "r");
-
-  if (fp == NULL)
-    ferr("Something went wrong while opening control scheme.");
-  
-  char ch;
-  unsigned int count = 0, i = 0;
-
-  while ( (unsigned char)(ch = fgetc(fp)) != (unsigned char)EOF ) {
-    count++;
-    if (count > 1000)
-      ferr("control scheme is certainly not over 1000 characters long.");
-  }
-
-  if (fseek(fp, 0L, SEEK_SET) != 0)
-    ferr("Something went wrong repositioning file.");
-
-  char *json_str = calloc(count+1, sizeof(char));
-  while ( (unsigned char)(ch = fgetc(fp)) != (unsigned char)EOF) {
-    json_str[i] = ch;
-    i++;
-  }
-
-  fclose(fp);
-
-  info("parsing control scheme");
-  cJSON *json = cJSON_Parse(json_str);
-  free(json_str);
-
-  if (json == NULL) {
-    const char *eptr = cJSON_GetErrorPtr();
-    char err_mesg[1000] = {0};
-    if (eptr != NULL)
-      sprintf(err_mesg, "JSON parse failed before %s", eptr);
-    else
-      sprintf(err_mesg, "JSON parse failed.");
-    cJSON_Delete(json);
-    ferr(err_mesg);
-  }
-
-  cJSON *control_scheme_name_json = cJSON_GetObjectItem(json, "name");
-  if (cJSON_IsString(control_scheme_name_json) && (control_scheme_name_json->valuestring != NULL)) {
-    td->control_scheme = calloc(strlen(control_scheme_name_json->valuestring)+1, sizeof(char));
-    strcpy(td->control_scheme, control_scheme_name_json->valuestring);
-  }
-  else {
-    cJSON_Delete(json);
-    ferr("Control scheme json must name a scheme.\n  e.g. { ... \"name\": \"constant\" ... }");
-  }
-  
-  int schemeidx = ctlidx_from_str(td->control_scheme);
-  if (schemeidx < 0) {
-    cJSON_Delete(json);
-    ferr("Control scheme json must name a known scheme.\n  i.e. \"constant\" or \"pid\"");
-  }
-
-  control_params_t *params = malloc(sizeof(control_params_t));
-  params->c = 0.0;
-  params->kp = 0.0;
-  params->ki = 0.0;
-  params->kd = 0.0;
-  params->set_point = 0.0;
-  params->sleep_ns = 100*1000*1000;
-
-  if (schemeidx == ctlidx_from_str("constant")) {
-    get_control_scheme_parameter(json, "c", "constant", "output value, double", &params->c);
-  }
-  else {
-    get_control_scheme_parameter(json, "kp", "pid", "proportional control coefficient, double", &params->kp);
-    get_control_scheme_parameter(json, "ki", "pid", "integral control coefficient, double", &params->ki);
-    get_control_scheme_parameter(json, "kd", "pid", "derivative control coefficient, double", &params->kd);
-    get_control_scheme_parameter(json, "setpoint", "pid", "set point/target, double", &params->set_point);
-  }
-
-  // optional params
-  get_optional_control_scheme_parameter(json, "sleep_ns", "interval between control calculations, nanoseconds", NULL, &params->sleep_ns, NULL);
-
-  cJSON_Delete(json);
-
-  td->control_params = params;
-
-}
-
-
-
-
-void
 check_argc(int i, int argc) 
 {
   if (i >= argc) {
@@ -261,11 +100,14 @@ check_argc(int i, int argc)
   }
 }
 
+
+
+
 void
 parse_args(int argc, const char **argv, thread_data_t *td) 
 {
   td->tag = "DELME";
-  unsigned int cs_set = 0, l_set = 0;
+  unsigned int cs_set = 0, l_set = 0, d_set = 0;
 
   for (unsigned int i = 1; i < argc; i++) {
     if (EITHER(argv[i], "-l", "--length")) {
@@ -277,7 +119,7 @@ parse_args(int argc, const char **argv, thread_data_t *td)
     else if (EITHER(argv[i], "-c", "--control-scheme")) {
       i++;
       check_argc(i, argc);
-      parse_contol_scheme_string(td, argv[i]);
+      read_control_scheme(td, argv[i]);
       cs_set = 1;
     }
     else if (EITHER(argv[i], "-t", "--tag")) {
@@ -289,6 +131,12 @@ parse_args(int argc, const char **argv, thread_data_t *td)
       help();
       exit(0);
     }
+    else if (EITHER(argv[i], "-d", "--depth")) {
+      i++;
+      check_argc(i, argc);
+      td->fill_depth = atof(argv[i]);
+      d_set = 1;
+    }
     else {
       argerr("given unknown arg");
     }
@@ -297,19 +145,20 @@ parse_args(int argc, const char **argv, thread_data_t *td)
   if (getuid() != 0)
     argerr("Hardware PWM needs root.");
 
-  if (!cs_set || !l_set)
-    argerr("Need to specify both length and control scheme.");
+  if (!cs_set || !l_set || !d_set)
+    argerr("Length, control scheme, and depth are required parameters.");
 
-  // finished setting args
+  // finished setting args, display
   fprintf(stderr, 
       "  "BOLD"rheometer"RESET" v"VERSION"\n"
-      "  Running with options:\n"
-      "    tag: \"%s\"\n"
-      "    control scheme: %s\n"
-      "      c: %.3f\n"
-      "      kp: %.3f\n"
-      "      ki: %.3f\n"
-      "      kd: %.3f\n"
-      "    length: %us\n",
+      "\n"
+      "  Run options:\n"
+      "    "FGYELLOW"tag"RESET": \"%s\"\n"
+      "    "FGYELLOW"control scheme"RESET": %s\n"
+      "      "FGMAGENTA"c"RESET": %.3f\n"
+      "      "FGMAGENTA"kp"RESET": %.3f\n"
+      "      "FGMAGENTA"ki"RESET": %.3f\n"
+      "      "FGMAGENTA"kd"RESET": %.3f\n"
+      "    "FGYELLOW"length"RESET": %us\n",
       td->tag, td->control_scheme, td->control_params->c, td->control_params->kp, td->control_params->ki, td->control_params->kd, td->length_s);
 }
--- src/control.c
+++ src/control.c
@@ -1,13 +1,16 @@
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <wiringPi.h>
 
 #include "rheo.h"
+#include "cJSON.h"
 
 
 static const char *control_schemes[] = {
   "constant",
-  "pid",
-  ""
+  "pid"
 };
 
 
@@ -26,7 +29,7 @@ ctlidx_from_str(const char *s)
 
 
 void
-calculate_speed(thread_data_t *td) 
+calculate_control_indicators(thread_data_t *td) 
 {
   float dt_tot = 0.0;
   unsigned int count = 0;
@@ -39,7 +42,12 @@ calculate_speed(thread_data_t *td)
     }
   }
   float dt_av = dt_tot / ((float)count);
-  td->speed_ind = ((1.0/6.0) / dt_av) * 60.0; // RPM
+  float speed_hz = ((1.0/6.0) / dt_av); // rotations per second
+  td->speed_ind = speed_hz;
+  float strainrate_invs = speed_hz * 3.1415926 * 2.0 * RI / (RO - RI);
+  td->strainrate_ind = strainrate_invs;
+  float torque_Nm = STRESS_M * td->adc[TSTS_CHANNEL] + STRESS_C;
+
 }
 
 
@@ -93,7 +101,7 @@ ctl_thread_func(void *vtd)
 
   while ( (!td->stopped) && (!td->errored) ) {
 
-    calculate_speed(td);
+    calculate_control_indicators(td);
 
     unsigned int control_action = ctlfunc(td);
 
@@ -109,4 +117,169 @@ ctl_thread_func(void *vtd)
   }
 
   return NULL;
+}
+
+
+
+
+void
+get_control_scheme_parameter(cJSON *json, const char *paramname, const char *schemename, const char *description, double *value)
+{
+  cJSON *dbl = cJSON_GetObjectItem(json, paramname);
+
+  if (dbl == NULL) {
+    char err_mesg[1000] = {0};
+    sprintf(err_mesg, "%s requires a parameter \"%s\" (%s)", schemename, paramname, description);
+    cJSON_Delete(json);
+    ferr(err_mesg);
+  }
+
+  if (!cJSON_IsNumber(dbl)) {
+    char err_mesg[1000] = {0};
+    sprintf(err_mesg, "%s requires \"%s\" to be a number (%s)", schemename, paramname, description);
+  }
+  
+  (*value) = dbl->valuedouble;
+}
+
+
+
+
+void
+get_optional_control_scheme_parameter(cJSON *json, const char *paramname, const char *description, double *dbl_value, unsigned int *int_value, char **str_value)
+{
+  cJSON *param = cJSON_GetObjectItem(json, paramname);
+
+  if (param == NULL)
+    return;
+
+  if (cJSON_IsNumber(param)) {
+
+    if (dbl_value == NULL) {
+
+      if (int_value == NULL) {
+        char err_mesg[1000] = {0};
+        sprintf(err_mesg, "JSON parse error. \"%s\" is not expected to be a number (%s).", paramname, description);
+        ferr(err_mesg);
+      }
+      else {
+        (*int_value) = param->valueint;
+      }
+
+    }
+    else {
+      (*dbl_value) = param->valuedouble;
+    }
+
+  } 
+  else if (cJSON_IsString(param)) {
+
+    if (str_value == NULL) {
+        char err_mesg[1000] = {0};
+        sprintf(err_mesg, "JSON parse error. \"%s\" is not expected to be a string (%s).", paramname, description);
+        ferr(err_mesg);
+    }
+    else {
+      (*str_value) = param->valuestring;
+    }
+  }
+}
+
+
+
+void
+read_control_scheme(thread_data_t *td, const char *control_scheme_string)
+{
+
+  if (access(control_scheme_string, F_OK) == -1) {
+    argerr("control scheme must be a json file describing the scheme.");
+  }
+
+  FILE *fp = fopen(control_scheme_string, "r");
+
+  if (fp == NULL)
+    ferr("Something went wrong while opening control scheme.");
+  
+  char ch;
+  unsigned int count = 0, i = 0;
+
+  while ( (unsigned char)(ch = fgetc(fp)) != (unsigned char)EOF ) {
+    count++;
+    if (count > 1000)
+      ferr("control scheme is certainly not over 1000 characters long.");
+  }
+
+  if (fseek(fp, 0L, SEEK_SET) != 0)
+    ferr("Something went wrong repositioning file.");
+
+  char *json_str = calloc(count+1, sizeof(char));
+  while ( (unsigned char)(ch = fgetc(fp)) != (unsigned char)EOF) {
+    json_str[i] = ch;
+    i++;
+  }
+
+  fclose(fp);
+
+  cJSON *json = cJSON_Parse(json_str);
+  free(json_str);
+
+  if (json == NULL) {
+    const char *eptr = cJSON_GetErrorPtr();
+    char err_mesg[1000] = {0};
+    if (eptr != NULL)
+      sprintf(err_mesg, "JSON parse failed before %s", eptr);
+    else
+      sprintf(err_mesg, "JSON parse failed.");
+    cJSON_Delete(json);
+    ferr(err_mesg);
+  }
+
+  cJSON *control_scheme_name_json = cJSON_GetObjectItem(json, "name");
+  if (cJSON_IsString(control_scheme_name_json) && (control_scheme_name_json->valuestring != NULL)) {
+    td->control_scheme = calloc(strlen(control_scheme_name_json->valuestring)+1, sizeof(char));
+    strcpy(td->control_scheme, control_scheme_name_json->valuestring);
+  }
+  else {
+    cJSON_Delete(json);
+    ferr("Control scheme json must name a scheme.\n  e.g. { ... \"name\": \"constant\" ... }");
+  }
+  
+  int schemeidx = ctlidx_from_str(td->control_scheme);
+  if (schemeidx < 0) {
+    cJSON_Delete(json);
+    char *err_mesg = calloc(1000, sizeof(char));
+    strcat(err_mesg, "Control scheme JSON must name a known scheme. Known schemes:");
+    for (unsigned int i = 0; i < sizeof(control_schemes)/sizeof(char*); i++) {
+      strcat(err_mesg, "\n   - ");
+      strcat(err_mesg, control_schemes[i]);
+    }
+    ferr(err_mesg);
+    //ferr("Control scheme json must name a known scheme.\n  i.e. \"constant\" or \"pid\"");
+  }
+
+  control_params_t *params = malloc(sizeof(control_params_t));
+  params->c = 0.0;
+  params->kp = 0.0;
+  params->ki = 0.0;
+  params->kd = 0.0;
+  params->set_point = 0.0;
+  params->sleep_ns = 100*1000*1000;
+
+  if (schemeidx == ctlidx_from_str("constant")) {
+    get_control_scheme_parameter(json, "c", "constant", "output value, double", &params->c);
+  }
+  else {
+    get_control_scheme_parameter(json, "kp", "pid", "proportional control coefficient, double", &params->kp);
+    get_control_scheme_parameter(json, "ki", "pid", "integral control coefficient, double", &params->ki);
+    get_control_scheme_parameter(json, "kd", "pid", "derivative control coefficient, double", &params->kd);
+    get_control_scheme_parameter(json, "setpoint", "pid", "set point/target, double", &params->set_point);
+  }
+
+  // optional params
+  get_optional_control_scheme_parameter(json, "sleep_ns", "interval between control calculations, nanoseconds", NULL, &params->sleep_ns, NULL);
+
+  cJSON_Delete(json);
+
+  td->control_params = params;
+
 }
