24d342414360652da647a46f26d5c6e3b31be602
Chris <chrismjboyle@gmail.com>
2019-06-26 08:55:47 +0100

    Added defines for calibrations, geometry; added space for control variables in thread_data_t

--- src/rheo.h
+++ src/rheo.h
@@ -4,15 +4,27 @@
 
 // macros {{{
 
+// MISC
 #define VERSION "0.1"
+
+// ADC
 #define ADC_COUNT 8
+#define PND_CHANNEL 1
+#define TSTS_CHANNEL 2
+
+// OPT
 #define OPTENC_COUNT 3
+
+// GPIO
 #define PWM_PIN 18
+
+// CONTROL
 #define SPD_HIST 100
 
+// UI
 #define BOLD  "\033[1m"
 #define RESET "\033[0m"
-
+// FOREGROUND
 #define FGBLACK   "\033[30m"
 #define FGRED     "\033[31m"
 #define FGGREEN   "\033[32m"
@@ -21,7 +33,7 @@
 #define FGMAGENTA "\033[35m"
 #define FGCYAN    "\033[36m"
 #define FGWHITE   "\033[37m"
-
+// BACKGROUND
 #define BGBLACK   "\033[40m"
 #define BGRED     "\033[41m"
 #define BGGREEN   "\033[42m"
@@ -31,7 +43,16 @@
 #define BGCYAN    "\033[46m"
 #define BGWHITE   "\033[47m"
 
+// LOGS
 #define TAGDEFAULT "DELME"
+
+// STRESS CAL
+#define STRESS_M 1.0
+#define STRESS_C 1.0
+
+// GEOMETRY (m)
+#define RI 0.01505
+#define RO 0.01975
 
 // }}}
 // types, structs, and enums {{{
@@ -76,22 +97,27 @@ typedef struct thread_data_t {
   unsigned long *time_us;
   unsigned long *adc;
   float *temperature;
-
+  
+  // process control stuff
   float speed_ind;
+  float strainrate_ind;
+  float stress_ind;
+  float viscosity_ind;
   float **ptimes;
-
-  // run_data
-  unsigned int length_s;
   char *control_scheme;
   control_params_t *control_params;
   unsigned int last_ca;
+
+  // run_data
+  unsigned int length_s;
+  float fill_depth;
   const char *tag;
   char *log_pref;
   char **log_paths;
   FILE **opt_log_fps;
   unsigned int log_count;
 
-  // control stuff
+  // program control stuff
   adc_handle_t *adc_handle;
   unsigned int log_ready;
   unsigned int adc_ready;
@@ -129,6 +155,7 @@ void free_thread_data(thread_data_t *dat);
 // }}}
 // log.c {{{ 
 
+void save_run_params_to_json(thread_data_t *td);
 void *log_thread_func(void *rtd);
 
 // }}}
@@ -159,6 +186,7 @@ void opt_mark(thread_data_t *td, unsigned int i);
 double control_PID(double tuning[3], double input);
 int ctlidx_from_str(const char *s);
 void *ctl_thread_func(void *vtd);
+void read_control_scheme(thread_data_t *td, const char *control_scheme_string);
 
 // }}}
 // args.c {{{
