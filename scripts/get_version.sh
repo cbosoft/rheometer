fc315eac1c8085c57f9758af8e309307a1d94a86
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-05-15 14:38:56 +0100

    Fixed string equality check in version script

--- scripts/get_version.sh
+++ scripts/get_version.sh
@@ -5,7 +5,8 @@ DATE_LAST_COMMIT=$(git --no-pager log -1 --format=%cd)
 DATE_NO_DST=$(echo $DATE_LAST_COMMIT | awk '{$NF="";sub(/[ \t]+$/,"")}1')
 DATE=$(date --date="$DATE_NO_DST" +%Y%m%d)
 
-if [[ $BRANCH -eq "dev" ]]; then
+
+if [[ "$BRANCH" == "dev" ]]; then
   echo "${DATE}dev"
 else
   echo "${DATE}"
