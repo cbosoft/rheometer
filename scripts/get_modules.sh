570576d432b730caeee47c77340619e8e7713b86
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-05-16 10:48:05 +0100

    Fixed module get script
    Forgot to apply regex replace globally, d'oh!

--- scripts/get_modules.sh
+++ scripts/get_modules.sh
@@ -1,3 +1,3 @@
 SOURCE="$(ls src/control/controllers/*.c) $(ls src/control/setters/*.c)"
-MODULES=$(echo $SOURCE | sed 's,src/control/,,' | sed 's/\.c/\.so/')
+MODULES=$(echo $SOURCE | sed 's,src/control/,,g' | sed 's/\.c/\.so/g')
 echo $MODULES
