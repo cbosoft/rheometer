cd037f9de6389531d56138ceabd4eb3e6275888e
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-05-22 15:33:26 +0100

    Updated schedule alg comment

--- src/main/schedule.c
+++ src/main/schedule.c
@@ -11,6 +11,12 @@ int schedule_main(int argc, const char **argv)
   // TODO: parse_schedule_args(argc, argv, sd);
 
   // TODO: run rheometer under the desired set of parameters
+  // take in a series of controllers, setters, and parameters for both
+  // use to create a list of argv,argc
+  // use to run main() again under each parameter set
+  // use --quiet to cut output
+  // output run number/progress after each completed run
+  // change terminal title too?
 
   // TODO: free_schedule_data(sd);
   return 0;
