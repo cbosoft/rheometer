5c245ffe34201b6db6c1473b51e46d0306f31c1e
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-05-23 15:21:46 +0100

    Carry return values through

--- src/main/main.c
+++ src/main/main.c
@@ -13,17 +13,14 @@ int main(int argc, const char **argv)
   switch (ca->rc) {
 
     case RC_RUN:
-      run_main(argc, argv);
-      break;
+      return run_main(argc, argv);
 
     case RC_SCHEDULE:
-      schedule_main(argc, argv);
-      break;
+      return schedule_main(argc, argv);
 
     case RC_CONFIG:
-      config_main(argc, argv);
-      break;
+      return config_main(argc, argv);
   }
 
-  return 0;
+  return -1;
 }
