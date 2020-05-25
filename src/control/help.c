d7c66380e8f20b88d99577171e43459deb6541a1
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-05-25 19:58:05 +0100

    Improved colour consistency to aid readability

--- src/control/help.c
+++ src/control/help.c
@@ -8,12 +8,12 @@
 void control_help(void)
 {
   fprintf(stdout,"%s",
-      "  "BOLD"Control Schemes"RESET"\n"
+      "  "BOLD""FGMAGENTA"Control Schemes"RESET"\n"
       "    Control schemes are defined in JSON files (stored in the data/ directory).\n"
       "    These simple files dictate what controller and setter to use. See the json \n"
       "    files in the ./data/* directory for examples.\n"
       "\n"
-      "  "BOLD"Control Module"RESET"\n"
+      "  "BOLD""FGMAGENTA"Control Modules"RESET"\n"
       "    Control modules contain functions to set the motor supply voltage in response\n"
       "    to sensor data. These modules are dynamically linked shared objects so you\n"
       "    can add extra functionality as you wish. Modules found:\n"
@@ -32,7 +32,7 @@ void control_help(void)
 
 
   fprintf(stdout, "%s",
-      "  "BOLD"Setpoint (setter) Module"RESET"\n"
+      "  "BOLD""FGMAGENTA"Setpoint (setter) Modules"RESET"\n"
       "    Setpoint for the controller is set by the setter module. It takes in sensor \n"
       "    readings and time, then deciding the new setpoint. Modules found:\n"
       "\n");
